110f8f82861a2442b37af76fb366425dce525279
Charlie Davies <charlieadavies@gmail.com>
2020-06-03 14:21:43 +0100

    simple migrations up/down now functional

--- src/main/kotlin/com/kotlindiscord/database/migrator/MigrationsManager.kt
+++ src/main/kotlin/com/kotlindiscord/database/migrator/MigrationsManager.kt
@@ -28,7 +28,7 @@ class MigrationsManager {
     /**
      * Gets the id of the migration which was most recently applied to the database.
      */
-    val currentMigrationId: Long?
+    private val currentMigrationId: Long?
         get() {
             var maxMigrationId: Long? = null
             transaction {
@@ -46,15 +46,15 @@ class MigrationsManager {
     /**
      * Map of the user created migrations against their IDs (the time at which they were created).
      */
-    val migrationsMap: Map<Long, AbstractMigration> by lazy {
+    private val migrationsMap: Map<Long, AbstractMigration> by lazy {
         // TODO handle the case where we find a non migration object more elegantly
         val reflections = Reflections("com.kotlindiscord.database.migrations")
 
         val classes = reflections.getSubTypesOf(AbstractMigration::class.java)
+
         @Suppress("MagicNumber")
         val migrationIds = classes.map { it.name.takeLast(13).toLong() }
         val maybeMigrations = classes.map { it.constructors.first().newInstance() }
-
         if (maybeMigrations.all { it is AbstractMigration }) {
             migrationIds.zip(maybeMigrations as List<AbstractMigration>).toMap()
         } else mapOf()
@@ -69,34 +69,79 @@ class MigrationsManager {
      * for the next migration class that hasn't been applied yet, and apply that.
      */
     fun migrateUp() {
-        TODO()
+        check(migrationsMap.isNotEmpty()) { throw IllegalStateException("No migrations found.") }
+        check(currentMigrationId != migrationsMap.keys.max()) {
+            throw IllegalStateException("Database fully migrated.")
+        }
+
+        val sortedMigrationKeys: List<Long> = migrationsMap.keys.sorted()
+        var nextMigrationId: Long
+        val nextMigration =
+            if (currentMigrationId == null) {
+                nextMigrationId = sortedMigrationKeys.min()!!
+                migrationsMap[nextMigrationId]
+            } else {
+                nextMigrationId = sortedMigrationKeys[sortedMigrationKeys.indexOf(currentMigrationId as Long) + 1]
+                migrationsMap[nextMigrationId]
+            }
+        checkNotNull(nextMigration) { throw IllegalStateException("There was a problem migrating.") }
+        transaction {
+            nextMigration.migrateUp()
+            SchemaUtils.create(Migrations)
+            val migrationToApply =
+                Migration.findById(nextMigrationId) ?: Migration.new(nextMigrationId) { applied = true }
+            migrationToApply.applied = true
+        }
     }
 
     /**
      * Migrates the database down a step by calling the `down` method on the currently applied migration.
      */
     fun migrateDown() {
-        TODO()
+        val currentMigrationObj = migrationsMap[currentMigrationId]
+        checkNotNull(currentMigrationObj)
+        transaction {
+            currentMigrationObj.migrateDown()
+            Migration[currentMigrationId as Long].applied = false
+        }
     }
 
     /**
      * Moves the database to the most recent migration, applying all migrations in order.
+     *
+     * If this method has been called, the database should be able to migrate up at least once. If not, the user has
+     * done something wrong.
      */
     fun migrateAll() {
-        TODO()
+        migrateUp()
+        while (true) {
+            try {
+                migrateUp()
+            } catch (e: IllegalStateException) {
+                break
+            }
+        }
+
     }
 }
 
 
 /* Temporary main function for testing */
 fun main() {
-    Database.connect(
+    val db = Database.connect(
         "jdbc:postgresql://" + System.getenv("DB_URL"),
         driver = "org.postgresql.Driver",
         user = System.getenv("DB_USER"),
         password = System.getenv("DB_PASSWORD")
     )
-
-    val obs = MigrationsManager().migrationsMap
-    println(obs)
+    db.useNestedTransactions = true
+    val manager = MigrationsManager()
+    manager.migrateUp()
+    manager.migrateUp()
+    manager.migrateDown()
+    manager.migrateDown()
+    manager.migrateAll()
+    manager.migrateDown()
+    manager.migrateDown()
+    manager.migrateAll()
 }
