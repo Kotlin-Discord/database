a9863da5444f46c59e40928647c323d0540cfbd9
Charlie Davies <charlieadavies@gmail.com>
2020-06-06 14:10:41 +0100

    moved table creation to migration

---
+++ src/main/kotlin/com/kotlindiscord/database/migrations/Migration1591121527450.kt
@@ -0,0 +1,26 @@
+package com.kotlindiscord.database.migrations
+
+import com.kotlindiscord.database.*
+import com.kotlindiscord.database.migrator.AbstractMigration
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
+class Migration1591121527450 : AbstractMigration {
+    override fun migrateUp() {
+        transaction {
+            exec("CREATE TYPE InfractionTypes AS ENUM ('Kick', 'Ban', 'Warn');")
+            SchemaUtils.create(Roles, Users, Infractions)
+            SchemaUtils.create(UserRoles)
+        }
+
+    }
+
+    override fun migrateDown() {
+        transaction {
+            SchemaUtils.drop(UserRoles)
+            SchemaUtils.drop(Roles, Users, Infractions)
+            exec("DROP TYPE InfractionTypes")
+        }
+
+    }
+}
