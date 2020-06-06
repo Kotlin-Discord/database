637f0588c94b23f2b096181d7bf59a2b90b0d0c0
Charlie Davies <charlieadavies@gmail.com>
2020-06-06 14:50:24 +0100

    adding logging backend

--- src/main/kotlin/com/kotlindiscord/database/DatabaseConnector.kt
+++ src/main/kotlin/com/kotlindiscord/database/DatabaseConnector.kt
@@ -1,11 +1,18 @@
 package com.kotlindiscord.database
 
+import io.sentry.Sentry
+import mu.KotlinLogging
 import org.jetbrains.exposed.sql.Database
 
 /**
- * Establish a connection to the database which can be used for exposed.
+ * Establish a connection to the database and logging settings.
  */
-fun connectToDb() {
+fun startDbConnection() {
+    val logger = KotlinLogging.logger {}
+    if (System.getenv().getOrDefault("SENTRY_DSN", null) != null) {
+        val sentry = Sentry.init()
+    }
+    logger.info { "Conne" }
     val db = Database.connect(
         "jdbc:postgresql://" + System.getenv("DB_URL"),
         driver = "org.postgresql.Driver",
