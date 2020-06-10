4d2ee03a91a303eef273a7e048dcb07a32cbdf76
Charlie Davies <charlieadavies@gmail.com>
2020-06-10 14:30:21 +0100

    actually fixed whitespace this time around

--- src/main/kotlin/com/kotlindiscord/database/connections.kt
+++ src/main/kotlin/com/kotlindiscord/database/connections.kt
@@ -8,7 +8,6 @@ import org.jetbrains.exposed.sql.Database
  * Establish a connection to the database and set up logging.
  */
 fun startDbAndLogger() {
-
     val logger = KotlinLogging.logger {}
     if (System.getenv().getOrDefault("SENTRY_DSN", null) != null) {
         val sentry = Sentry.init()
@@ -21,6 +20,7 @@ fun startDbAndLogger() {
         password = System.getenv("DB_PASSWORD")
     )
     db.useNestedTransactions = true
+
     logger.info { "Successfully connected to database." }
 }
 
@@ -28,13 +28,11 @@ fun startDbAndLogger() {
  * Utility function to connect to the database.
  */
 fun connectToDb() {
-
     val db = Database.connect(
         "jdbc:postgresql://" + System.getenv("DB_URL"),
         driver = "org.postgresql.Driver",
         user = System.getenv("DB_USER"),
         password = System.getenv("DB_PASSWORD")
     )
-
     db.useNestedTransactions = true
 }
